2af01a0c085c33ff59c705ca57f4a280603ef64b
Ayrton Guerra <ayrton.guerra@eemovel.com>
2025-08-12 12:23:46 -0300

    refactor: add state to update

--- src/modules/movies/components/movies-list/container.tsx
+++ src/modules/movies/components/movies-list/container.tsx
@@ -3,18 +3,20 @@ import { MoviesPresenter } from "./presenter"
 import { getMoviesService } from "../../services/movies-list-service"
 import { useEffect, useState } from "react"
 import type { MoviesData } from "../../types/movies"
+import { useMovieList } from "../../contexts/movies-list-context"
 
 export const MoviesContainer = () => {
+  const { filters } = useMovieList()
   const [movies, setMovies] = useState<MoviesData | null>(null)
 
   useEffect(() => {
     fetchMovies()
-  }, [])
+  }, [filters])
 
   const fetchMovies = async () => {
     try {
       setMovies(null)
-      const movies = await getMoviesService()
+      const movies = await getMoviesService(filters)
       setMovies(movies)
     } catch (err) {
       console.log(err)
--- src/modules/movies/components/movies-list/presenter.tsx
+++ src/modules/movies/components/movies-list/presenter.tsx
@@ -1,9 +1,12 @@
 import type { MoviesData } from "../../types/movies"
 import { ScrollArea } from "@/shared/components/ui/scroll-area"
-import { Loader } from "lucide-react"
+import { Loader, Plus } from "lucide-react"
 import { ColumnLayout } from "@/shared/layouts/column-layout"
 import { TableComponent } from "../table"
 import { ListSkeleton } from "../list-skeleton"
+import { PaginationList } from "../pagination-list"
+import { Button } from "@/shared/components/ui/button"
+import { SearchFilterComponent } from "../search-filter"
 
 type Props = {
   movies: MoviesData | null
@@ -14,7 +17,16 @@ export const MoviesPresenter = ({ movies }: Props) => {
     <ColumnLayout>
       <ScrollArea>
         <div className="w-4xl flex flex-col gap-4">
+          <div className="flex justify-between gap-4">
+            <SearchFilterComponent />
+
+            <Button>
+              <Plus />
+              Adiciona filme
+            </Button>
+          </div>
           {!!movies && <TableComponent movies={movies.results} />}
+          {!!movies && <PaginationList />}
 
           {!movies && <ListSkeleton />}
         </div>
