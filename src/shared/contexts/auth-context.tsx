b25e8d42ca393910c7e0f2802be44c5e235e49da
Ayrton Guerra <ayrton.guerra@eemovel.com>
2025-08-11 21:48:42 -0300

    feat: create auth context

---
+++ src/shared/contexts/auth-context.tsx
@@ -0,0 +1,42 @@
+import { createContext, useContext, useEffect, useState, type ReactNode } from "react"
+
+type LoginContextType = {
+  user: string | null
+  login: (credentials: string) => void
+  logout: () => void
+}
+
+const AuthContext = createContext<LoginContextType | undefined>(undefined)
+
+export const LoginProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<string | null>(null)
+
+  useEffect(() => {
+    const token = localStorage.getItem("token")
+
+    if (token) {
+      try {
+        setUser(token)
+      } catch {
+        logout()
+      }
+    }
+  }, [])
+
+  const login = (credentials: string) => {
+    localStorage.setItem("token", credentials)
+  }
+
+  const logout = () => {
+    localStorage.removeItem("token")
+  }
+
+  return <AuthContext.Provider value={{ user, login, logout }}>{children}</AuthContext.Provider>
+}
+
+export const useLogin = () => {
+  const context = useContext(AuthContext)
+  if (!context) throw new Error("useLogin must used inside of the LoginProvider")
+
+  return context
+}
