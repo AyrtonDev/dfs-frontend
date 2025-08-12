46cd2d963ada401737e17d02e56637c31aa3d719
Ayrton Guerra <ayrton.guerra@eemovel.com>
2025-08-12 13:19:44 -0300

    feat: set default return if filter is null

--- src/modules/movies/services/movies-list-service.ts
+++ src/modules/movies/services/movies-list-service.ts
@@ -2,9 +2,9 @@ import { api } from "@/shared/config/api"
 import type { AxiosError } from "axios"
 import type { FilterType } from "../schemas/filter"
 
-export const getMoviesService = async (filter: FilterType | null = {}) => {
+export const getMoviesService = async (filter: FilterType | null) => {
   try {
-    const result = await api.post("/movies", filter)
+    const result = await api.post("/movies", filter ? filter : {})
     return result.data
   } catch (err) {
     const axiosError = err as AxiosError<{ error: string }>
