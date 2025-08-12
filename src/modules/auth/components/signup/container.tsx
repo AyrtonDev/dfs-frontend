57807df71bb1433f3091de0369e98e64456fe55a
Ayrton Guerra <ayrton.guerra@eemovel.com>
2025-08-11 22:56:58 -0300

    feat: create form signup

--- src/modules/auth/components/signup/container.tsx
+++ src/modules/auth/components/signup/container.tsx
@@ -1,5 +1,21 @@
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { signUpFormSchema, type SignUpFormType } from "../../schemas/signup"
 import { SignUpPresenter } from "./presenter"
 
 export const SignUpContainer = () => {
-  return <SignUpPresenter />
+  const signUpForm = useForm<SignUpFormType>({
+    resolver: zodResolver(signUpFormSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+    },
+  })
+
+  const handleSignUp = ({ email, password, name, passwordConfirmation }: SignUpFormType) => {
+    console.log(email, password, name, passwordConfirmation)
+  }
+  return <SignUpPresenter form={signUpForm} submit={handleSignUp} />
 }
--- src/modules/auth/components/signup/presenter.tsx
+++ src/modules/auth/components/signup/presenter.tsx
@@ -1,3 +1,93 @@
-export const SignUpPresenter = () => {
-  return <div></div>
+import { Card, CardContent, CardHeader, CardTitle } from "@/shared/components/ui/card"
+import type { SignUpFormType } from "../../schemas/signup"
+import { type UseFormReturn, type SubmitHandler } from "react-hook-form"
+import { Button } from "@/shared/components/ui/button"
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "@/shared/components/ui/form"
+import { Input } from "@/shared/components/ui/input"
+import { CenteredLayout } from "@/shared/layouts/centered-layout"
+
+type Props = {
+  form: UseFormReturn<SignUpFormType>
+  submit: SubmitHandler<SignUpFormType>
+}
+
+export const SignUpPresenter = ({ form, submit }: Props) => {
+  return (
+    <CenteredLayout>
+      <Card className="max:h-3xl w-80">
+        <CardHeader>
+          <CardTitle>Cadastro</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form className="flex flex-col gap-4" onSubmit={form.handleSubmit(submit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Nome</FormLabel>
+                    <FormControl>
+                      <Input {...field} placeholder="João" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>E-mail</FormLabel>
+                    <FormControl>
+                      <Input {...field} placeholder="teste@mail.com" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Senha</FormLabel>
+                    <FormControl>
+                      <Input {...field} placeholder="******" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="passwordConfirmation"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Confirmação da senha</FormLabel>
+                    <FormControl>
+                      <Input {...field} placeholder="igual a senha" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <Button type="submit" className="w-full">
+                Criar cadastro
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </CenteredLayout>
+  )
 }
