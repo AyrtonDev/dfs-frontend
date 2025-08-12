a0be5d6f0c8b470124c642ddd154a3285476a619
Ayrton Guerra <ayrton.guerra@eemovel.com>
2025-08-12 16:53:26 -0300

    fix: change class style

--- src/shared/layouts/column-layout.tsx
+++ src/shared/layouts/column-layout.tsx
@@ -3,8 +3,8 @@ import type { BasicPropsType } from "../types/props"
 
 export const ColumnLayout = ({ children }: BasicPropsType) => {
   return (
-    <div className="min:h-screen relative flex w-screen justify-center">
-      <div className="max:w-5xl my-4 flex flex-col">{children}</div>
+    <div className="relative flex min-h-screen w-screen justify-center">
+      <div className="my-4 flex w-full max-w-5xl flex-col">{children}</div>
       <Menu />
     </div>
   )
