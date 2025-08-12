e01afc9fb90ba94ee3a0da53dc40b7f5e63a7c33
Ayrton Guerra <ayrton.guerra@eemovel.com>
2025-08-12 17:57:27 -0300

    feat: make form register movie schema

---
+++ src/modules/movies/schemas/form-movie.ts
@@ -0,0 +1,15 @@
+import z from "zod"
+
+export const formMovieSchema = z.object({
+  title: z.string().min(4, "Titulo obrigatorio"),
+  originalTitle: z.string().optional(),
+  imageUrl: z.url(),
+  description: z.string().optional(),
+  releaseDate: z.date("Data de lançamento obrigatoria"),
+  duration: z.string("Duração obrigatorio"),
+  budge: z.string().optional(),
+  genre: z.string().min(4, "Genêro obrigratorio"),
+  director: z.string().optional(),
+})
+
+export type FormMovieType = z.infer<typeof formMovieSchema>
