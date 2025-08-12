3d142211f43ff44912bc96cf40dfb6022243bb3e
Ayrton Guerra <ayrton.guerra@eemovel.com>
2025-08-11 23:56:13 -0300

    feat: add sonner in app component

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { LoginPage } from "@/modules/auth/page/login"
-import { AuthProvider } from "./shared/contexts/auth-context"
-import { SignUpPage } from "./modules/auth/page/signup"
-import { PublicRoute } from "./shared/components/public-route"
+import { AuthProvider } from "@/shared/contexts/auth-context"
+import { SignUpPage } from "@/modules/auth/page/signup"
+import { PublicRoute } from "@/shared/components/public-route"
+import { Toaster } from "@/shared/components/ui/sonner"
 
 export const App = () => {
   return (
@@ -14,6 +15,7 @@ export const App = () => {
             <Route path="/cadastro" element={<SignUpPage />} />
           </Route>
         </Routes>
+        <Toaster />
       </BrowserRouter>
     </AuthProvider>
   )
